d1e5264970df325ee5343bfd21bba55912c89647
Davi Arcos Baptista <cl1621@fiap.com.br>
2019-07-30 15:26:33 -0300

    baptixta | new animations

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,11 +1,13 @@
 const description = document.querySelector('.description');
-const skills = document.querySelector('.skills');
+const skillsTitle = document.querySelector('.skills');
+const skillsList = document.querySelectorAll('.skills > ul > li');
 const socialMedia = document.querySelectorAll('.social-media > a');
 
 // Animation - Timeline (Desktop)
 const tl = new TimelineMax();
 tl.fromTo(description, .7, {opacity:0, x: -50}, {opacity:1, x: 0, ease: Back.easeOut.config(1.7,), delay: .4});
-tl.fromTo(skills, .7, {opacity:0, y: -30}, {opacity:1, y: 0, ease: Circ.easeOut, delay: .2});
+tl.fromTo(skillsTitle, .7, {opacity:0, y: -30}, {opacity:1, y: 0, ease: Circ.easeOut, delay: .2});
+tl.staggerFromTo(skillsList, .2, {opacity:0, x: -5}, {opacity:1, x: 0}, .145); 
 tl.staggerFromTo(socialMedia, .2, {opacity:0, x: -5, y: -12}, {opacity:1, x: 0, y: 0}, .145); 
 
 // Animation - Timeline (Mobile)
