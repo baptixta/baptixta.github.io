b51751dbc9a7b46874e3975bb3f96c880412a220
baptixta <daviarcosbaptista@gmail.com>
2020-03-12 18:48:38 -0300

    baptixta | side content done

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,13 +1,11 @@
-const description = document.querySelector('.description');
-const skillsTitle = document.querySelector('.skills');
-const skillsList = document.querySelectorAll('.skills > ul > li');
+const title = document.querySelectorAll('.me > div > h1');
+const text = document.querySelectorAll('.me > div > p');
 const socialMedia = document.querySelectorAll('.social-media > a');
 
 // Animation - Timeline (Desktop)
 const tl = new TimelineMax();
-tl.fromTo(description, .7, {opacity:0, x: -50}, {opacity:1, x: 0, ease: Back.easeOut.config(1.7,), delay: .4});
-tl.fromTo(skillsTitle, .7, {opacity:0, y: -30}, {opacity:1, y: 0, ease: Circ.easeOut, delay: .2});
-tl.staggerFromTo(skillsList, .2, {opacity:0, x: -5}, {opacity:1, x: 0}, .145); 
-tl.staggerFromTo(socialMedia, .2, {opacity:0, x: -5, y: -12}, {opacity:1, x: 0, y: 0}, .145); 
+tl.fromTo(title, .6, {opacity:0, y: -25}, {opacity:1, y: 0}, 0);
+tl.staggerFromTo(text, .4, {opacity: 0, x: -5}, {opacity: 1, x: 0, delay: .4}, .2, 0)
+tl.staggerFromTo(socialMedia, .5, {opacity:0, x: -8}, {opacity:1, x: 0, delay: .7}, .15, 0); 
 
 // Animation - Timeline (Mobile)
