c86ed3f0b858774467e91f94b5093265189d2813
baptixta <daviarcosbaptista@gmail.com>
2020-03-12 21:53:49 -0300

    baptixta | portfolio - avalon

--- assets/js/index.js
+++ assets/js/index.js
@@ -3,9 +3,9 @@ const text = document.querySelectorAll('.me > div > p');
 const socialMedia = document.querySelectorAll('.social-media > a');
 
 // Animation - Timeline (Desktop)
-const tl = new TimelineMax();
-tl.fromTo(title, .6, {opacity:0, y: -25}, {opacity:1, y: 0}, 0);
+const tl = new TimelineMax({delay: .3});
+tl.fromTo(title, .4, {opacity:0, y: -25}, {opacity:1, y: 0}, 0);
 tl.staggerFromTo(text, .4, {opacity: 0, x: -5}, {opacity: 1, x: 0, delay: .4}, .2, 0)
-tl.staggerFromTo(socialMedia, .5, {opacity:0, x: -8}, {opacity:1, x: 0, delay: .7}, .15, 0); 
+tl.staggerFromTo(socialMedia, .3, {opacity:0, x: -8}, {opacity:1, x: 0, delay: .7}, .1, 0); 
 
 // Animation - Timeline (Mobile)
